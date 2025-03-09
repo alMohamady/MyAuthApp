6d622471f2e5b12771ae65c40589736868661810
mohamady-div <major_2047@hotmail.com>
2025-03-09 16:01:10 +0200

    Lesson 06

--- MyAuthApp/Areas/Identity/Data/AppUser.cs
+++ MyAuthApp/Areas/Identity/Data/AppUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -9,5 +10,16 @@ namespace MyAuthApp.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the AppUser class
 public class AppUser : IdentityUser
 {
+    [PersonalData]
+    [Column(TypeName ="varchar(50)")]
+    public string FriestName { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "varchar(50)")]
+    public string LastName { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "varchar(20)")]
+    public string MobileNumber { get; set; }
 }
 
